93fa196d1e5bb09f1ce38e432a1a1ab98ac4227f
Deniel <denielborioni1@gmail.com>
2021-08-16 21:34:31 +0200

    aggiungo main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import pygame, sys
+import random
+
+pygame.init()
+
+sfondo=pygame.image.load('immagini/sfondo.png')
+uccello=pygame.image.load('immagini/uccello.png')
+base=pygame.image.load('immagini/base.png')
+gameover=pygame.image.load('immagini/gameover.png')
+tubo_giu=pygame.image.load('immagini/tubo.png')
+tubo_su=pygame.transform.flip(tubo_giu,False,True)
+
+SCHERMO=pygame.display.set_mode((288,512))
+FPS=60/1
+FLY_UP = -10
+DIFFICOLTA = 2
+VEL_AVANZ= 3
+clock = pygame.time.Clock()
+
+def disegna_oggetti():    
+    SCHERMO.blit(sfondo, (0,0) )
+    SCHERMO.blit(uccello, (uccellox, uccelloy))
+    SCHERMO.blit(base, (basex,400))
+
+def aggiorna() :
+    pygame.display.update()
+    clock.tick(FPS)
+
+def inizzializza():
+    global uccellox, uccelloy, uccello_vely  # definisco 3 variabili globali
+    global basex
+    uccellox, uccelloy = 60, 150  # imposto la posizione iniziale
+    uccello_vely = 0       
+    basex =0  
+
+inizzializza()
+
+while True:
+    basex-= VEL_AVANZ
+    if basex < -45: basex = 0
+    print(basex)
+    uccello_vely +=0.3 * DIFFICOLTA
+    uccelloy += uccello_vely
+    numeri = []
+    #for nnn in numeri:
+    #    print(nnn)    
+
+    for event in pygame.event.get(): # get = prendi  set = imposta
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_UP): # evento di tipo "tasto schiacciato" e tasto "freccia su"
+            uccello_vely = FLY_UP
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    disegna_oggetti()
+    aggiorna()
