8bc708440fda5507cf9d27ad65cf97bb08bb5c5a
Deniel <denielborioni1@gmail.com>
2021-08-20 17:49:55 +0200

    corretto percorso

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame, sys
 
 pygame.init()
 
-sfondo=pygame.image.load('--immagini/sfondo.png') # C:\Programmare\Python\pygame-imparare-1 + immagini/sfondo.png 
+sfondo=pygame.image.load('immagini/sfondo.png') # C:\Programmare\Python\pygame-imparare-1 + immagini/sfondo.png 
 #sfondo=pygame.image.load('c:/miei file/cartella1/alex//immagini/sfondo.png') percorso assoluto
 uccello=pygame.image.load('immagini/uccello.png')
 base=pygame.image.load('immagini/base.png')
